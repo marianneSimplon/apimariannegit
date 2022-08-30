43304293203677387cbeac71866a64e551c1d5c8
marianne depierre <marianne.depierre.simplon@gmail.com>
2022-08-30 15:32:02 +0200

    adding unit tests

---
+++ tests/test_fast.py
@@ -0,0 +1,7 @@
+import unittest
+from model import IrisModel, IrisSpecies
+
+class TestIrisModel(unittest.TestCase):
+    def test_model_initialization(self):
+        new_model = IrisModel()
+        self.assertIn('iris_model.joblib',new_model.model_fname_)
